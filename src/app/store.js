c2a6deb6bf22f62c74e324a379a7e75a4b167027
Maksym Morenko <morenko.maxim@gmail.com>
2022-08-26 22:31:08 +0300

    базовое использование middlware + logger

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,5 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
 
 const counter = (state = 0, action) => {
     switch (action.type) {
@@ -17,8 +18,45 @@ const counter = (state = 0, action) => {
     }
 };
 
-export const store = createStore(counter,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //для запуска расширения React Developer Tools
+// подключение мидлвери происходит в момент создания стора
+// создадим мидлвер myLogger (логирование), это цепочка вызовов функций, 
+// где store объект с всеми доступными ему методами store.getState(), store.dispatch()
+// next обязательно должен быть внутри мидлвера, в него мы передаем текущее событие (action)
+
+// в качестве мидлвейра можно использовать разные библиотеки, воспользуемся redux-logger
+// npm i redux-logger
+// как правило мидлвейры нужны на этапе тестирования или разроботчика, на этапе продакшна они не нужны
+// можно запустить милдлвери только на любом режиме, выборочно
+// режим разработчика
+// process.env.NODE_ENV === "development" 
+
+// создадим массив функций мидлверов, в нем бедем собирать все наши мидлверы, в нем, по умолчанию уже могут храниться статичные мидлверы
+// но в данном случае у нас их нет, добавим туда логер при условии режима разраб
+const middleware = [];
+
+// сделаем проверку, если мы в режиме разрабротчика, то добавляем в наш массив мидлверов логер
+if (process.env.NODE_ENV === "development") {
+  middleware.push(logger);
+}
+
+const myLogger = (store) => (next) => (action) => {
+    console.log("dispatched an action", action.type); //распечатаем инфо при экшине и укажем его тип
+    next(action);
+    // строчки 33, 34 выполняются синхронно
+    // после некста можно выполнить еще другие децствия, достанем текущее значение стейта из стора store.getState()
+    console.log("updated state is", store.getState());
+};
+
+// для подключения мидлвейри, нужно воспользоваться хелпером applyMiddleware, импортируем его из редакса 
+// после єтого в криейтСтор, вторім или третим парамитром передаем enhaencer (усилитель), в данном случае это applyMiddleware()
+// и передаем в него наши функции myLogger. передаем их, не вызываем. А дальше редакс выстраивает их в общую цепочку
+
+
+export const store = createStore(
+    counter,
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(...middleware), // можно добавить сразу несколько аргументов (разные мидлвейр)
+    // воспользуемся спред оператором так как нам нужны функции из массива
 );
 
 // actions for hooks
